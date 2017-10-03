7891decaae4978018c6b7732ee3dfa2da67c931e
CSharp Code Guru <32496110+codegurucsharp@users.noreply.github.com>
2017-10-03 14:12:56 -0700

    First code (#1)
    * first committ
    
    * First Merge sort code committ
    
    Signed-off-by: VIBHOR JAIN <vibhorj@microsoft.com>

---
+++ Sorting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int[] A = new[] { 9, 5, 6, 4, 8, 2, 7, 1 };
+            // using Method 1
+            MergeSortImpl impl = new MergeSortImpl();
+            impl.MergeSort(A,0, A.Length-1);
+            Utilities.printArray(A,0,A.Length-1);
+
+            Console.WriteLine("This is the second method of sorting using MergeSort.");
+
+            int[] B = new[] { -9, 5, -6, 4, -8, 2, -7, 1 };
+            // using Method 2            
+            impl.MergeSort2(B,0, B.Length-1);
+            Utilities.printArray(B,0,B.Length-1);
+
+            Console.WriteLine("");
+
+            Console.ReadLine();
+        }
+    }
+}
